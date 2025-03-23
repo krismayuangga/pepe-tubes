d22f35b5e13b6807a03bfec93dc9065949b16eac
BSE Projects <groupbse1@gmail.com>
2025-03-23 11:01:55 +0700

    PEPEToken.sol

--- contracts/PEPEToken.sol
+++ contracts/PEPEToken.sol
@@ -14,11 +14,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PEPEToken is ERC20, Ownable {
+    // Define max supply constant (100 billion tokens)
+    uint256 public constant MAX_SUPPLY = 100_000_000_000 * 10**18;
+
     constructor() ERC20("PEPE Token", "PEPE") Ownable(msg.sender) {
-        _mint(msg.sender, 100_000_000_000 * 10 ** decimals()); // 100 Billion PEPE
+        // Mint entire supply to deployer
+        _mint(msg.sender, MAX_SUPPLY);
     }
 
+    // Instead, we'll explicitly override it to disable minting
+    function mint(address, uint256) public pure {
+        revert("PEPEToken: minting disabled - fixed supply");
+    }
+    
+    // Allow users to burn their own tokens
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+    
+    // Add informational getter to show max supply
+    function getMaxSupply() public pure returns (uint256) {
+        return MAX_SUPPLY;
     }
 }
